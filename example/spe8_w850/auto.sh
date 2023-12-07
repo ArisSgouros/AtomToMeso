85f987e4d96705e3ff74e9e3f93d1c8148b4725f
ArisSgouros <arissgouros@gmail.com>
2023-12-07 11:44:41 +0200

    test(spe8_w850): calculate bond/angle dist

--- example/spe8_w850/auto.sh
+++ example/spe8_w850/auto.sh
@@ -6,13 +6,11 @@ EVFRAME=1
 
 python atom_to_meso.py $path_a2m > o.log
 
-#python ../../source/lmp_type_strip.py o.cg.lammpstrj 3 "[5]" > o.cg.strip_5.lammpstrj
-#
-#python ../../source/lmp_angle_dist_v3.py o.cg.dat [1] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_angle_dist_v3.py o.cg.dat [2] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_angle_dist_v3.py o.cg.dat [3] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_angle_dist_v3.py o.cg.dat [4] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_bond_dist_v3.py o.cg.dat [1] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_bond_dist_v3.py o.cg.dat [2] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_bond_dist_v3.py o.cg.dat [3] $n_frame o.cg.lammpstrj
-#python ../../source/lmp_bond_dist_v3.py o.cg.dat [4] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_bond.py o.cg.dat [1] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_bond.py o.cg.dat [2] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_bond.py o.cg.dat [3] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_bond.py o.cg.dat [4] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_angle.py o.cg.dat [1] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_angle.py o.cg.dat [2] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_angle.py o.cg.dat [3] $n_frame o.cg.lammpstrj
+python $path_a2m/dist_angle.py o.cg.dat [4] $n_frame o.cg.lammpstrj
