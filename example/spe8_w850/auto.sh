408b592bc9e9a2e7927f448c59e2fa86ed645f7e
ArisSgouros <arissgouros@gmail.com>
2024-02-13 13:29:24 +0200

    test(spe8_w850): refresh

--- example/spe8_w850/auto.sh
+++ example/spe8_w850/auto.sh
@@ -5,12 +5,3 @@ n_frame=40
 EVFRAME=1
 
 python atom_to_meso.py $path_a2m > o.log
-
-#python $path_a2m/compute_bond.py o.cg.dat [1] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_bond.py o.cg.dat [2] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_bond.py o.cg.dat [3] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_bond.py o.cg.dat [4] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_angle.py o.cg.dat [1] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_angle.py o.cg.dat [2] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_angle.py o.cg.dat [3] $n_frame o.cg.lammpstrj
-#python $path_a2m/dist_angle.py o.cg.dat [4] $n_frame o.cg.lammpstrj
